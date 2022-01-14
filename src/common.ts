7fded397854e39b4e0e36003b978cd1c5549d99e
alephao <7674479+alephao@users.noreply.github.com>
2022-01-13 22:35:18 -0300

    feat: add real genzee owner count

--- src/common.ts
+++ src/common.ts
@@ -1,11 +1,16 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 import { GenzeeStats, User } from "../generated/schema";
 
+export const genzeeContractAddress =
+  "0x201675fBFAAAC3A51371E4C31FF73Ac14ceE2A5A";
+export const oddxContractAddress = "0x4095547F958593B5431C0306e81df4293991d5B3";
+
 export const loadGenzeeStats: () => GenzeeStats = () => {
   let stats = GenzeeStats.load("v1");
   if (!stats) {
     stats = new GenzeeStats("v1");
     stats.totalGenzeesStaked = 0;
+    stats.totalGenzeeOwners = 0;
   }
   return stats;
 };
--- src/genzee.ts
+++ src/genzee.ts
@@ -1,8 +1,23 @@
 import { Transfer } from "../generated/Genzee/Genzee";
 import { GenzeeToken } from "../generated/schema";
-import { loadOrCreateUser } from "./common";
+import {
+  loadGenzeeStats,
+  loadOrCreateUser,
+  oddxContractAddress,
+} from "./common";
 
 export function handleTransfer(event: Transfer): void {
+  // Skip this fn if transfering from or to ODDX.
+  // This means it's unstaking or staking which is handled on oddworx.ts
+  if (
+    event.params.to.toHexString().toLowerCase() ==
+      oddxContractAddress.toLowerCase() ||
+    event.params.from.toHexString().toLowerCase() ==
+      oddxContractAddress.toLowerCase()
+  ) {
+    return;
+  }
+
   let token = GenzeeToken.load(event.params.tokenId.toString());
   if (!token) {
     token = new GenzeeToken(event.params.tokenId.toString());
@@ -12,7 +27,14 @@ export function handleTransfer(event: Transfer): void {
   token.owner = event.params.to.toHexString();
   token.save();
 
+  let stats = loadGenzeeStats();
+
   let userTo = loadOrCreateUser(event.params.to.toHexString());
+
+  if (userTo.genzeeBalance == 0) {
+    stats.totalGenzeeOwners++;
+  }
+
   userTo.genzeeBalance++;
   userTo.save();
 
@@ -20,10 +42,17 @@ export function handleTransfer(event: Transfer): void {
     event.params.from.toHexString() ==
     "0x0000000000000000000000000000000000000000"
   ) {
+    stats.save();
     return;
   }
 
   let userFrom = loadOrCreateUser(event.params.from.toHexString());
   userFrom.genzeeBalance--;
+
+  if (userFrom.genzeeBalance == 0) {
+    stats.totalGenzeeOwners++;
+    stats.save();
+  }
+
   userFrom.save();
 }
